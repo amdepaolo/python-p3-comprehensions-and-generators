1e9bc945769fd2755583e4cb25405319a2539304
Adam DePaolo <amdepaolo@gmail.com>
2023-09-30 17:27:39 -0400

    solves tests

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens_list = [num for num in num_list if num%2 == 0]
+    print (evens_list)
+    return evens_list
 
 def make_exclamation(sentence_list):
-    pass
+    loud_list = [sentence + "!" for sentence in sentence_list]
+    return loud_list
